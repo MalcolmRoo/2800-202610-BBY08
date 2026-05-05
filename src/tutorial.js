121a4c9be22b5cb8c4160125e2ad6920e7f1b4e9
Malcolm Roosdahl <malcoroo@gmail.com>
2026-05-04 23:29:26 -0700

    Tutorial Testing

---
+++ src/tutorial.js
@@ -0,0 +1,6 @@
+var tutorialButton = document.getElementById("startTutorial");
+
+tutorialButton.addEventListener("click", (event) => {
+    var tutorial = document.getElementById("Tutorial");
+    tutorial.style.display = "inline";
+})
