7b204cb6289374ab7f21bc09d1e1d0f77b66180c
Malcolm Roosdahl <malcoroo@gmail.com>
2026-05-05 16:18:01 -0700

    moved tutorial popup and added testing text

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,6 +1,16 @@
+
+
 var tutorialButton = document.getElementById("startTutorial");
 
 tutorialButton.addEventListener("click", (event) => {
     var tutorial = document.getElementById("Tutorial");
+    var infoContainer = document.getElementById("textContainer");
     tutorial.style.display = "inline";
+
+    var html = "";
+
+    html += "<p>This is some testing text to see how the text container handles a large amount of text" +
+    "which is just a bunch of random ramblings!!!!";
+
+    infoContainer.innerHTML = html;
 })
