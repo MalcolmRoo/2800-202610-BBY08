bed57f4972bc3b8e2816eba5fa9dad56c60d446b
Malcolm Roosdahl <malcoroo@gmail.com>
2026-05-05 18:28:06 -0700

    Testing adding HTML through tutorial.js

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,5 +1,3 @@
-
-
 var tutorialButton = document.getElementById("startTutorial");
 
 tutorialButton.addEventListener("click", (event) => {
@@ -7,10 +5,34 @@ tutorialButton.addEventListener("click", (event) => {
     var infoContainer = document.getElementById("textContainer");
     tutorial.style.display = "inline";
 
-    var html = "";
+    var textElement = document.createElement("p");
+    textElement.textContent = "Welcome to GreenScan! Let me show you around the place, how are you going to idenify your plant?"
+    var listElement = document.createElement("ul");
+    
+    var captureBullet = document.createElement("li");
+    var captureButton = document.createElement("button");
+    captureButton.textContent = "Take a photo";
+    captureBullet.appendChild(captureButton);
 
-    html += "<p>This is some testing text to see how the text container handles a large amount of text" +
-    "which is just a bunch of random ramblings!!!!";
+    var uploadBullet = document.createElement("li");
+    uploadBullet.textContent = "Upload a photo"
 
-    infoContainer.innerHTML = html;
+    var textBullet = document.createElement("li");
+    textBullet.textContent = "Search by name"
+
+    listElement.appendChild(captureBullet);
+    listElement.appendChild(uploadBullet);
+    listElement.appendChild(textBullet);
+    
+    textElement.style.margin = "12px";
+    textElement.style.padding = "3px";
+    listElement.style.marginLeft = "35px";
+
+    captureBullet.style.paddingBottom = "8px";
+    uploadBullet.style.paddingBottom = "8px";
+    textBullet.style.paddingBottom = "8px";
+
+    infoContainer.appendChild(textElement);
+    infoContainer.appendChild(listElement);
+    tutorialButton.style.display = "none";
 })
