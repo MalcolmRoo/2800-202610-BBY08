53b628a4b9199f53323b18621b9f0e818cf9a50b
Malcolm Roosdahl <malcoroo@gmail.com>
2026-05-05 20:15:19 -0700

    Basic Tutorial popups completed

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,38 +1,53 @@
 var tutorialButton = document.getElementById("startTutorial");
+var nextButton = document.createElement("button");
+var tutorial = document.getElementById("Tutorial");
+var infoContainer = document.getElementById("textContainer");
+var textElement = document.createElement("p");
+
+let stage = 0;
 
 tutorialButton.addEventListener("click", (event) => {
-    var tutorial = document.getElementById("Tutorial");
-    var infoContainer = document.getElementById("textContainer");
     tutorial.style.display = "inline";
-
-    var textElement = document.createElement("p");
-    textElement.textContent = "Welcome to GreenScan! Let me show you around the place, how are you going to idenify your plant?"
-    var listElement = document.createElement("ul");
+    tutorial.style.background = "darkkhaki";
     
-    var captureBullet = document.createElement("li");
-    var captureButton = document.createElement("button");
-    captureButton.textContent = "Take a photo";
-    captureBullet.appendChild(captureButton);
-
-    var uploadBullet = document.createElement("li");
-    uploadBullet.textContent = "Upload a photo"
-
-    var textBullet = document.createElement("li");
-    textBullet.textContent = "Search by name"
-
-    listElement.appendChild(captureBullet);
-    listElement.appendChild(uploadBullet);
-    listElement.appendChild(textBullet);
+    textElement.textContent = "Welcome to GreenScan! Let me show you around the place!"
     
     textElement.style.margin = "12px";
     textElement.style.padding = "3px";
-    listElement.style.marginLeft = "35px";
-
-    captureBullet.style.paddingBottom = "8px";
-    uploadBullet.style.paddingBottom = "8px";
-    textBullet.style.paddingBottom = "8px";
+    textElement.style.color = "cornsilk";
 
     infoContainer.appendChild(textElement);
-    infoContainer.appendChild(listElement);
+
+    nextButton.textContent = "Next";
+    nextButton.style.padding = "10px";
+    nextButton.style.marginLeft = "126px";
+    nextButton.style.marginBottom = "5px";
+    nextButton.style.background = "darkolivegreen";
+    nextButton.style.borderRadius = "8px";
+    nextButton.style.color = "darkkhaki";
+
+    infoContainer.appendChild(nextButton);
+
     tutorialButton.style.display = "none";
-})
+});
+
+nextButton.addEventListener("click", (event) => {
+    stage ++;
+    if(stage === 1){
+        tutorial.style.transform = "translate(410px, -173px)";
+        textElement.textContent = "You can click on this little gear to change the settings!"
+    } else if (stage === 2){
+        tutorial.style.transform = "translate(-374px, 247px)";
+        textElement.textContent = "You can click on this button to use your device's camera to take a picture of the plant in question; great for if you have the plant in front of you!";
+    }  else if (stage === 3){
+        tutorial.style.transform = "translate(-374px, 350px)";
+        textElement.textContent = "You can click on this button to upload a picture you've already taken; great for if you want to identify a plant you've seen before!";
+    }  else if (stage === 4){
+        tutorial.style.transform = "translate(374px, 350px)";
+        textElement.textContent = "You can click on this button to search by name; great for if you wanna know more about a plant you already know of!";
+    } else if (stage === 5){
+        tutorial.style.transform = "translate(0px, -100px)";
+        textElement.textContent = "Please selection the option you feel best suits your needs!"
+        nextButton.style.display = "none";
+    }
+});
