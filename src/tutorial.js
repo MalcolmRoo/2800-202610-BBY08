f1e4990cd0f39eae87d89708861c62548aa9f732
Malcolm Roosdahl <malcoroo@gmail.com>
2026-05-06 22:41:40 -0700

    Added mobile friendly tutorial placement

--- src/tutorial.js
+++ src/tutorial.js
@@ -36,25 +36,41 @@ tutorialButton.addEventListener("click", (event) => {
 nextButton.addEventListener("click", (event) => {
     stage ++;
     if(stage === 1){
-        tutorial.style.transform = "translate(26rem, -22rem)";
+        if(screen.width < 1275){
+            tutorial.style.transform = "translate(1rem, -22rem)";
+        } else {
+            tutorial.style.transform = "translate(26rem, -22rem)";
+        }
         textElement.textContent = "You can click on this little gear to change the settings!"
     } else if (stage === 2){
-        tutorial.style.transform = "translate(-23rem, 4rem)";
+        if(screen.width < 1275){
+            tutorial.style.transform = "translate(1rem, -4rem)";
+        } else {
+            tutorial.style.transform = "translate(-23rem, 4rem)";
+        }
         textElement.textContent = "You can click on this button to use your device's camera to take a picture of the plant in question; great for if you have the plant in front of you!";
     }  else if (stage === 3){
-        tutorial.style.transform = "translate(-23rem, 11rem)";
+        if(screen.width < 1275){
+            tutorial.style.transform = "translate(1rem, 21rem)";
+        } else {
+            tutorial.style.transform = "translate(-23rem, 11rem)";
+        }
         textElement.textContent = "You can click on this button to upload a picture you've already taken; great for if you want to identify a plant you've seen before!";
     }  else if (stage === 4){
-        tutorial.style.transform = "translate(23rem, 11rem)";
+        if(screen.width < 1275){
+            tutorial.style.transform = "translate(-1rem, 21rem)";
+        } else {
+            tutorial.style.transform = "translate(23rem, 11rem)";
+        }
         mascotImg.style.transform = "translateX(305px)";
         textElement.textContent = "You can click on this button to search by name; great for if you wanna know more about a plant you already know of!";
     } else if (stage === 5){
-        tutorial.style.transform = "translate(0px, -17rem)";
+        tutorial.style.transform = "translate(1rem, -17rem)";
         mascotImg.style.transform = "translateX(-65px)";
         textElement.textContent = "When Foraging always make sure you are taking a plant in small enough quantities to not harm the local ecosystem. When eating anything you forage it is a good idea to double check your find with multiple sources, use our identification as one of your sources and make sure to always double check the information you recieve. We do not take responsibility for what any user of our app does with the information provided, please forage responsibly.";
         nextButton.textContent = "I Agree";
     } else if (stage === 6) {
-        tutorial.style.transform = "translate(0px, -22rem)";
+        tutorial.style.transform = "translate(1rem, -22rem)";
         textElement.textContent = "Please select how you would like to identify the plant from one of the buttons below!"
         nextButton.style.display = "none";   
     }
